81243869259510015798fcd23832ee5af2928126
Jack-Milewski <144130362+Jack-Milewski@users.noreply.github.com>
2023-10-20 12:44:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const poll = document.querySelector('.poll')
+const buttons = poll.querySelectorAll('button')
+
+buttons[0].addEventListener('click', vote);
+buttons[1].addEventListener('click', vote);
+buttons[2].addEventListener('click', vote);
+buttons[3].addEventListener('click', vote);
+function vote(e) {
+    console.log(e.target.innerHTML)
+}
